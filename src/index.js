f50e2e50cc51da75c7f01e5c198d99c838b4868b
MamorukunBE <mamorukunmobile@gmail.com>
2021-05-12 16:17:56 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// const LazyComponent = React.lazy(() => import("./lazycomponent"));
+const LazyComponent = React.lazy(() => { return (new Promise((resolve) => setTimeout(resolve, 1 * 1000)).then(() => import("./lazycomponent"))); });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class LazyTest extends React.Component {
+	render() {
+		return (
+			<div>
+				<Suspense fallback={<div>Loading...</div>}>
+					<React.Fragment>
+						<LazyComponent />
+						Simple display
+					</React.Fragment>
+				</Suspense>
+			</div>
+		);
+	}
+}
+ReactDOM.render(<LazyTest />, document.getElementById('root'));
